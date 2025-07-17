957c88b2b7572b48556a231faf9502eae9762a66
Sophie-Ndobe <139694211+Sophie-Ndobe@users.noreply.github.com>
2025-07-17 15:41:34 +0200

    Created the setInterval function for the selected city updates

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ function timeDateUpdate() {
     let parisDateElement = document.querySelector("#par-date");
     let updatedParisDate = moment.tz("Europe/Paris").format("MMMM Do, YYYY");
 
-    if(parisDateElement) {
+    if (parisDateElement) {
         parisDateElement.innerHTML = updatedParisDate;
     }
 
     let parisTimeElement = document.querySelector("#par-time");
     let updatedParisTime = moment.tz("Europe/Paris").format("H:mm:ss [<small>] A[</small>]");
 
-    if(parisTimeElement) {
+    if (parisTimeElement) {
         parisTimeElement.innerHTML = updatedParisTime;
     }
 
@@ -18,36 +18,49 @@ function timeDateUpdate() {
     let shanghaiDateElement =  document.querySelector("#sha-date")
     let updatedShanghaiDate = moment.tz("Asia/Shanghai").format("MMMM Do, YYYY");
 
-    if(shanghaiDateElement) {
+    if (shanghaiDateElement) {
         shanghaiDateElement.innerHTML = updatedShanghaiDate;
     }
 
     let shanghaiTimeElement = document.querySelector("#sha-time");
     let updatedShanghaiTime = moment.tz("Asia/Shanghai").format("H:mm:ss [<small>] A[</small>]");
 
-    if(shanghaiTimeElement) {
+    if (shanghaiTimeElement) {
         shanghaiTimeElement.innerHTML = updatedShanghaiTime;
     }
 }
 
+let selectedCityInterval;
+
 function updateTimeDate(event){
     let changedCity = event.target.value;
 
-    let cityName = changedCity.replace("_", " ").split("/")[1];
-    console.log(cityName);
-    let changedCityDate = moment().format("MMMM Do, YYYY");
-    let changedCityTime = moment.tz(changedCity).format("H:mm:ss [<small>] A[</small>]");
+    if (selectedCityInterval) {
+        clearInterval(selectedCityInterval);
 
-    let citiesUpdateElement = document.querySelector("#cities-update")
-    citiesUpdateElement.innerHTML = `
-        <div class="city">
-            <div>
-                <h2>${cityName}</h2>
-                <div class="date">${changedCityDate}</div>
+    }
+
+
+    function updatedSelectedCityInfor() {
+        let cityName = changedCity.replace("_", " ").split("/")[1];
+        //console.log(cityName);
+        let changedCityDate = moment().format("MMMM Do, YYYY");
+         let changedCityTime = moment.tz(changedCity).format("H:mm:ss [<small>] A[</small>]");
+
+        let citiesUpdateElement = document.querySelector("#cities-update")
+        citiesUpdateElement.innerHTML = `
+            <div class="city">
+                <div>
+                    <h2>${cityName}</h2>
+                    <div class="date">${changedCityDate}</div>
+                </div>
+                <div class="time">${changedCityTime}</div>
             </div>
-            <div class="time">${changedCityTime}</div>
-            </div>
-    `
+         `;
+    }
+
+    updatedSelectedCityInfor();
+    selectedCityInterval = setInterval(updatedSelectedCityInfor, 1000);
 }
 
 timeDateUpdate();
