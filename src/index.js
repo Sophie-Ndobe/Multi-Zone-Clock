e374acbf157a987c4e7fd559f049af60c5721251
Sophie-Ndobe <139694211+Sophie-Ndobe@users.noreply.github.com>
2025-07-17 14:53:27 +0200

    Updated the functionality for the select options dropdown

--- src/index.js
+++ src/index.js
@@ -20,36 +20,33 @@ function timeDateUpdate() {
 
 function updateTimeDate(event){
     let changedCity = event.target.value;
+
+    let cityName = changedCity.replace("_", " ").split("/")[1];
+    console.log(cityName);
     let changedCityDate = moment().format("MMMM Do, YYYY");
     let changedCityTime = moment.tz(changedCity).format("H:mm:ss [<small>] A[</small>]");
 
-    //console.log(changedCityTime);
-
     let citiesUpdateElement = document.querySelector("#cities-update")
-    /*
     citiesUpdateElement.innerHTML = `
         <div class="city">
             <div>
-                <h2>${changedCity}</h2>
+                <h2>${cityName}</h2>
                 <div class="date">${changedCityDate}</div>
             </div>
             <div class="time">${changedCityTime}</div>
             </div>
     `
-    */
-   citiesUpdateElement.innerHTML = `
-        <h2>${changedCity}</h2>
-        <div>${changedCityDate}</div>
-        <div>This is the round clock code</div>
-        <div>${changedCityTime}</div>
-
-   `
-
-
 }
 
 timeDateUpdate();
 setInterval(timeDateUpdate, 1000);
 
+
+
 let selectElement = document.querySelector("#cities");
 selectElement.addEventListener("change", updateTimeDate);
+
+
+
+
+
